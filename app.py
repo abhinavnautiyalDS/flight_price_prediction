b8c8644498f5e5abf60ef5e33c68efdd9c86f851
Abhinav Nautiyal <164336356+abhinavnautiyalDS@users.noreply.github.com>
2025-04-04 23:17:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import numpy as np
 import pickle
 import base64
 import gdown
-pip install sklearn
+pip install --no-cache-dir scikit-learn
+
 
 if not os.path.exists(".installed_dependencies"):  # Run only once
     os.system("pip install -r requirements.txt")
